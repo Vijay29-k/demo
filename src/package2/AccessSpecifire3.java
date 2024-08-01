599d60b454d94c96992327ad702759b6d14b7c4c
Vijay29-k <odemevijaykumar@gmail.com>
2024-08-01 14:45:11 +0530

    new branch commit

--- src/package2/AccessSpecifire3.java
+++ src/package2/AccessSpecifire3.java
@@ -8,7 +8,8 @@ public class AccessSpecifire3 {
 	public static void main(String[] args) {
 		
 		System.out.println(AccessSpecifire1.a);
-		
+		System.out.println("New branch commit");
+		System.out.println("New branch commit2");
 		AccessModifire obj = new AccessModifire();
 		
 		System.out.println(obj.n1);
